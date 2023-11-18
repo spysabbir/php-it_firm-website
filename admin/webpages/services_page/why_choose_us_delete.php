040051e01c4c25c7e45e0b4746f17c8a9811840d
spysabbir <sabbirahammedsovon@gmail.com>
2023-11-18 10:15:32 +0600

    Fix

--- admin/webpages/services_page/why_choose_us_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once "../../db.php";
-
-// Service Delete
-spy_sabbir_delete_with_photo("choose_us", "id", $_GET['id'], "choose_us", "why_choose_us_cover_photo");
-$_SESSION['deleted_status'] = "Working Process Deleted Successfully!";
-header('location: why_choose_us.php');
-?>
